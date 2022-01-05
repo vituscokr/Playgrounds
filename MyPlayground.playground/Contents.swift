a45e49f8beb18e7c6caf48f7461db8e7df213a90
Kyeongtae Nam <vituscokr@gmail.com>
2022-01-05 09:39:42 +0900

    playground 임시로 ar 코드 추가합니다.

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,43 @@
 import UIKit
 
-var greeting = "Hello, playground"
+import ARKit
+import RealityKit
+import PlaygroundSupport
+
+
+
+
+//let arView = ARView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+//
+//let mesh = MeshResource.generateBox(size: 0.2)
+//let material = SimpleMaterial(color: .blue, roughness: 0.5, isMetallic: true)
+//let modelEntity = ModelEntity(mesh: mesh, materials: [material])
+//
+//let anchorEntity = AnchorEntity()
+//anchorEntity.addChild(modelEntity)
+//arView.scene.addAnchor(anchorEntity)
+
+//https://medium.com/geekculture/realitykit-911-swift-playgrounds-or-how-to-create-and-debug-ar-apps-on-ipad-pro-190a6d2e03bb
+//https://stackoverflow.com/questions/60289680/is-there-a-good-way-to-implement-realitykit-into-a-swift-playground
+//https://developer.apple.com/kr/augmented-reality/quick-look/
+
+
+//let arView = ARView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+//arView.environment.background = .color(.black)
+//
+//let fileURL = Bundle.main.url(forResource: "LunarRover_English", withExtension: "reality")
+//let bowlingScene = try! Entity.load(contentsOf: fileURL!)
+//
+//let anchor = AnchorEntity()
+//anchor.addChild(bowlingScene)
+//anchor.scale = [4,4,4]
+//anchor.position.y = -0.5
+//
+//arView.scene.anchors.append(anchor)
+//
+//
+//PlaygroundPage.current.setLiveView(arView)
+
+
+
+
