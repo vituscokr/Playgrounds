60630991743b83e11a1d22f3ccb24cd3c9b2502d
vitus <vituscokr@gmail.com>
2022-06-30 09:10:05 +0900

    라이브 뷰 추가 및 combine operators 추가중

--- MyPlayground.playground/Pages/Combine.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Combine.xcplaygroundpage/Contents.swift
@@ -7,6 +7,142 @@ import UIKit
 var subscriptions = Set<AnyCancellable>()
 
 
+example(of: "drop(while:)") {
+    let numbers = (1...10).publisher
+    numbers
+        .drop(while: {
+            $0 % 5 != 0
+        })
+        .sink(receiveCompletion : { finished  in
+            print("completion \(finished)")
+        }, receiveValue: {
+            print($0)
+        })
+        .store(in : &subscriptions)
+}
+
+example(of: "dropFirst") {
+    let numbers = (1...10).publisher
+    numbers
+        .dropFirst(8)
+        .sink(receiveCompletion : { finished  in
+            print("completion \(finished)")
+        }, receiveValue: {
+            print($0)
+        })
+        .store(in : &subscriptions)
+}
+
+example(of: "last(where:)") {
+    let number = PassthroughSubject<Int,Never>()
+    
+    number
+        .last(where: {
+            $0 % 2 == 0
+        })
+        .sink(receiveCompletion : { finished  in
+            print("completion \(finished)")
+        }, receiveValue: {
+            print($0)
+        })
+        .store(in : &subscriptions)
+    
+    number.send(1)
+    number.send(2)
+    number.send(3)
+    number.send(4)
+    number.send(5)
+    //finished
+    number.send(completion: .finished)
+}
+
+example(of: "last(where:)") {
+    let number = (1...9).publisher
+    number
+        .print("numbers")
+        .last(where: {
+            $0 % 2 == 0
+        })
+        .sink(receiveCompletion : { finished  in
+            print("completion \(finished)")
+        }, receiveValue: {
+            print($0)
+        })
+        .store(in : &subscriptions)
+    
+
+    
+    
+}
+
+
+
+example(of: "first(where:)") {
+    let number = (1...9).publisher
+    number
+        .print("numbers")
+        .first(where: {
+            $0 % 2 == 0
+        })
+        .sink(receiveCompletion : { finished  in
+            print("completion \(finished)")
+        }, receiveValue: {
+            print($0)
+        })
+        .store(in : &subscriptions)
+}
+
+example(of: "ignoreOutput") {
+    let numbers = (1...10_000).publisher
+    numbers
+        .ignoreOutput()
+        .sink(receiveCompletion : { finished  in
+            print("completion \(finished)")
+        }, receiveValue: {
+            print($0)
+        })
+        .store(in : &subscriptions)
+}
+/// nil is filtered by copmactMap
+example(of: "compactMap") {
+    let strings = ["a", "1.24","3","def","45","0.23"].publisher
+    
+    strings
+        .compactMap {Float($0)}
+        .sink (receiveValue: {
+            print($0)
+        })
+        .store(in : &subscriptions)
+}
+
+example(of: "removeDuplicates") {
+    
+    let words = "hey hey there! want to listen to mister mister ?"
+        .components(separatedBy: " ")
+        .publisher
+    
+    words
+        .removeDuplicates()
+        .sink (receiveValue: {
+            print($0)
+        })
+        .store(in: &subscriptions)
+    
+}
+
+example(of: "filter") {
+    let number = (1...10).publisher
+    
+    number
+        .filter { $0.isMultiple(of: 3) }
+        .sink(receiveValue: { n in
+            print("\(n) is a multiple of 3 ")
+        })
+        .store(in: &subscriptions)
+}
+
+
+
 example(of: "async / await") {
     let subject = CurrentValueSubject<Int, Never>(0)
     Task {
